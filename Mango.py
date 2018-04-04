b2fdeed9d2af7f4005b48231a0c45516fca630bc
JavaLeg <jamesz96.au@gmail.com>
2018-04-04 15:49:01 +1000

    Reworked some of the OO-stuff. Encoding of new data looks to work out fine. User defined audio parameters may not be necessary as encoding sets the parameters on the medium data

--- Mango.py
+++ Mango.py
@@ -1,52 +1,15 @@
-from PIL import Image, ImageOps
-from pydub import AudioSegment
-from Mcoder import mc
-import numpy
-import soundfile as sf
 
 class Mango:
-
-	def __init__(self, medium):
+	def __init__(self, data, medium, out):
+		self.__data = data
 		self.__medium = medium
-		self.__pixels = 30
-		self.__mc = mc(medium)
+		self.__out = out
 
-		# 44100 is an industry standard sampling rate
-		self.__wav_rate = 44100
+	def getData(self):
+		return self.__data
 
+	def getMedium(self):
+		return self.__medium
 
-	def Mangonise(self):
-		self.__inputSanity()
-		medium_data = self.__d.decode()
-
-
-
-	def __inputSanity(self):
-
-		format = self.__medium[-3:]
-
-		if format == 'wav' or format == 'flac' or format == 'ogg':
-			# Native file format
-			return
-		elif format == 'mp3':
-
-			print("mp3 file detected!\n"
-				+ "Converting to "
-				+ self.__medium[-3:] + ".wav...")
-
-			# Convert into wav
-			sound = AudioSegment.from_mp3(self.__medium)
-
-			# Modify the mango to read from new file
-			self.__medium = self.__medium[:-3] + "wav"
-			sound.export(self.__medium, format="wav")	
-		else:
-			print(format + " is not a supported mango.")
-			
-		return
-
-	def set_sample(self, rate):
-		self.__wav_rate = rate
-
-	def set_pixel(self, rate):
-		self.__pixels = rate
+	def getOut(self):
+		return self.__out
--- make.py
+++ make.py
@@ -1,26 +1,39 @@
 import argparse
+from Curator import Curator
 from Mango import Mango
+from Mcoder import Mcoder
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("data", help="Data file to be hidden.")
+	parser.add_argument("input", help="Data file to be hidden.")
 	parser.add_argument("medium", help="The audio file.")
+	parser.add_argument("output", help="Set the output file/path.")
 	parser.add_argument("-p", "--pixels", help="Pixels per second. Default value: 30.", type=int)
 	parser.add_argument("-s", "--sampling", help="Sampling rate. Default value: 44100.", type=int)
 	args = parser.parse_args()
 
+	# Initialise Curator
+	c = Curator()
+
+	d_input, medium = c.Organise(args.input, args.medium)
+
 	# Initialise Mango
-	mango = Mango(args.medium)
+	mango = Mango(d_input, medium, args.output)
+
+	# Initialise Mcoder
+	# Handles the decoding/encoding process for mango obj type
+	mc = Mcoder(mango)
 
 	# Set user defined parameters if necessary
-	if args.pixels:
-		mango.set_pixels(args.pixels)
+	# if args.pixels:
+	# 	mango.set_pixels(args.pixels)
 
-	if args.sampling:
-		mango.set_sample(args.sampling)
+	# if args.sampling:
+	# 	mango.set_sample(args.sampling)
 
 	# Get down and boogy
-	mango.Mangonise(args.data)
+	mc.juice()
+
 
 if __name__ == "__main__":
 	main()
